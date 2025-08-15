ba6bafcd8ac41a6045584163f79b05caeddc2cdc
mdfaiz9 <mdfaizalam827@gmail.com>
2025-08-15 08:46:20 +0530

    fix: Add console log for response in getAuthUser function for debugging

--- src/lib/api.js
+++ src/lib/api.js
@@ -19,6 +19,7 @@ export const logout = async () => {
 export const getAuthUser = async () => {
   try {
     const res = await axiosInstance.get("/auth/me");
+    console.log(res);
     return res.data;
   } catch (error) {
     console.log("Error in getAuthUser:", error);
